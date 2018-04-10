460e1c3f5f42e6265bbaa1d6baf3fb3652d43c20
xiaodong <xiaodong.yi@ottv.tv>
2018-04-10 15:29:32 +0800

    [hhh]

--- wp-content/themes/yusi1.0/404.php
+++ wp-content/themes/yusi1.0/404.php
@@ -1,19 +1,33 @@
 <?php get_header(); ?>
 <div class="content-wrap">
-	<div style="text-align:center;padding:10px 0;font-size:16px;background-color:#ffffff;">
-		<h2 style="font-size:36px;margin-bottom:10px;">哎哟～404了～休息一下，玩个游戏吧！</h2><script type="text/javascript">
-/*漂浮120*270，创建于2013-3-12*/
-var cpro_id = "u1232049";
-</script>
-<script src="http://cpro.baidustatic.com/cpro/ui/f.js" type="text/javascript"></script>
-<script type="text/javascript">
-/*580*90，创建于2013-3-17*/
-var cpro_id = "u1236691";
-</script>
-<script src="http://cpro.baidustatic.com/cpro/ui/c.js" type="text/javascript"></script>
-  <embed type="application/x-shockwave-flash" width="600" height="400" src="http://images.yusi123.com/zhuamao.swf" wmode="transparent" quality="high" scale="noborder" flashvars="width=600&amp;height=400" allowscriptaccess="sameDomain" align="L">
-<br>
-
-
+	<?php
+    for ($t=0;$t<360;$t++) {
+        $y=2*cos($t)-cos(2*$t);
+        $x=2*sin($t)-sin(2*$t);
+        $x+=3;
+        $y+=3;
+        $x*=70;
+        $y*=70;
+        $x=round($x);
+        $y=round($y);
+        $str[]=$x;
+        $y=$y+2*(180-$y);
+        $x=$y;
+        $str[]=$x;
+    }
+     $im=imagecreate(400, 400);//创建画布400*400
+     $black=imagecolorallocate($im, 255, 255, 255);
+     $red=imagecolorallocate($im, 255, 0, 0);//设置颜色
+     imagepolygon($im, $str, 360, $red);
+     imagestring($im, 5, 130, 160, date("Y-m-d h:i:s"), $red);
+     imagestring($im, 5, 130, 190, "Content is missing ", $red);
+     imagestring($im, 5, 195, 220, "But ", $red);
+     imagestring($im, 5, 165, 250, "i love you ", $red);
+     $resultNamePath = dirname(dirname(dirname(dirname(__FILE__)))).DIRECTORY_SEPARATOR.'love.jpg';
+     imagegif($im, $resultNamePath);//输出图片
+    ?>
+</div>
+<div style="text-align: center;">
+	<img src="/love.jpg"/>
 </div>
 <?php get_footer(); ?>
