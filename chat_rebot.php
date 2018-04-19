602501336923469215565fae8ef4b372d42cbc8c
xiaodong <xiaodong.yi@ottv.tv>
2018-04-19 22:03:11 +0800

    [chat]

--- chat_rebot.php
+++ chat_rebot.php
@@ -2,23 +2,23 @@
 //引入MeepoPS
 require_once (dirname(__FILE__).'/MeepoPS/MeepoPS/index.php');
 require_once (dirname(__FILE__).'/PhpCurl.php');
-
-$cu = new PhpCurl();
+$setopt = array('port' => '8080');
+$cu = new PhpCurl($setopt);
 //使用WebSocket协议传输的Api类
 $webSocket = new \MeepoPS\Api\Websocket('0.0.0.0', '19910');
 $webSocket->callbackNewData = function ($connect, $data) use ($cu){
     if(!trim($data)){
       $msg ='欢迎来闲聊！！！';
     }else {
-        $content = $cu->get('http://www.helshe.com:8080/api/chats', ['title'=>trim($data)]);
+        $content = $cu->get('http://www.helshe.com/api/chats', ['title'=>trim($data)]);
         $content = json_decode($content,true);
         if(empty($content)){
-          $msg ='你太调皮了！！！超出我的范围啦';
+          $msg ='你太调皮了！！！下次回答你';
         }else {
           if(array_key_exists('title', $content)){
             $msg = $content['title'];
           }else {
-            $msg ='没有查询任何信息，可能出现错误.';
+            $msg ='无法回复';
           }
         }
     }
